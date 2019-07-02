487fb200ddd8ecb683b70f0e2f6a4f7be0b813ee
Carlos Alexandre Bello <carlos.bello@el.com.br>
2019-07-01 22:59:58 -0300

    Project

--- PPMToolFullStack/src/main/java/br/com/bello/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/br/com/bello/ppmtool/services/ProjectService.java
@@ -34,4 +34,12 @@ public class ProjectService {
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+        if(project == null){
+            throw new ProjectIdException("Project ID: "+projectId.toUpperCase()+" does not exist!");
+        }
+        projectRepository.delete(project);
+    }
 }
